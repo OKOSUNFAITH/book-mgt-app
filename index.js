d998eb6ff49666e5779168e3692c43fd1f08ea20
OKOSUNFAITH <onowohme@gmail.com>
2025-05-21 11:38:19 +0100

    jwt&bcrypt

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require ('express')
-const app = express()
-;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const app = express();
 
 const PORT = 3000;
+app.use(express.json());
 
 const books = [
     {id: 1, title:'the old woman', desc:'kids', author:'John'},
@@ -11,9 +13,9 @@ const books = [
     {id: 4, title:'sunrise', desc:'kids', author:'Smith'},
 ]
 const users = [];
- 
+const JWT_SECRET="secret" ;
 
-app.use(express.json());
+
 
 app.get('/books/', (req, res)=>{
     res.send(books);
@@ -23,8 +25,8 @@ app.get('/books/', (req, res)=>{
 app.get('/books/:id', (req, res)=>{
     console.log(req.params.id);
 
-    const book = books.filter((book)=> book.id == req.params.id);
-
+    const book = books.find((book)=> book.id == parseInt(req.params.id));
+    if (!book) return res.status(404).json({ message: 'Book not found' });
     res.json(book)
 })
 
@@ -32,8 +34,9 @@ app.post('/books/', (req, res) => {
     const newBook = {
         id: books.length + 1,
         title: req.body.title,
-        desc: req.body.desc
-    }
+        desc: req.body.desc,
+        author: req.body.author
+    };
     books.push(newBook);
     res.json(newBook);
     
@@ -61,20 +64,50 @@ app.put('/books/:id', (req, res) => {
           res.json(book);
     })
 
-app.post('/sign-up', (req, res) => {
+app.post('/books/sign-up', async(req, res) => {
     const { username, email, password } = req.body;
-    const users = users.find(u => u.username === username);
-    if (users) {
+    const userExist = users.find(user => user.email === email);
+    if (userExist) {
         return res.status(400).json({ message: 'User already exists' });
     }
     if (!username || !email || !password) {
         return res.status(400).json({ message: 'All fields are required' });
     }
-       
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.status(201).json({ message: 'User created', user: { username } });
+    const newUser = { username, email, password: hashedPassword };
+    users.push(newUser);
+  
+    // const token = jwt.sign(
+    //     { email: newUser.email,  username: newUser.username },
+    //     JWT_SECRET,
+    //     { expiresIn: '1h' }
+    // );
+    res.status(201).json({message: 'user created successfully',
+        user: { username: newUser.username, email: newUser.email },
+        // token: token
+    });
 }
-);
+)
+
+ 
+app.post('/books/sign-in', async(req, res) => {
+    const {  email, password } = req.body;
+    const userExist = users.find(user => user.email === email);
+    if (!userExist) {
+        return res.status(400).json({ message: 'User does not exist' });
+    }
+    const ispasswordValid = await bcrypt.compare(password, userExist.password);
+    if (!ispasswordValid) {
+        return res.status(400).json({ message: 'Invalid password' });
+    }
+    const token = jwt.sign(
+        { email: userExist.email, username: userExist.username },
+        JWT_SECRET,
+        { expiresIn: '1h' }
+    );
+    res.status(200).json({ message: 'User signed in successfully', token , user: { username: userExist.username, email: userExist.email } });
+});
 
 
 
